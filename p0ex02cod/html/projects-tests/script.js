5c235aed3b413382a8548d479f74e7c50fcc7b13
traian_win10_x86 <traian@tutanota.com>
2023-10-25 20:48:34 -0300

    Uploading Git cheat sheet

--- p0ex02cod/html/projects-tests/script.js
+++
@@ -1,6 +0,0 @@
-const flexAuto = document.getElementById("flex-auto");
-const flexInitial = document.getElementById("flex-initial");
-flexAuto.addEventListener("click", () => {
-  flexInitial.style.display =
-    flexInitial.style.display === "none" ? "block" : "none";
-});
