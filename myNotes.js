fca3442f52c4dcc92d00ee3a2c69888a92321046
traian_win10_x86 <traian@tutanota.com>
2023-11-12 21:49:29 -0300

    myNotes.js update

--- myNotes.js
+++ myNotes.js
@@ -686,8 +686,19 @@ bill = 430;
 tip = bill >= 50 && bill <= 300 ? bill * .15 : bill * .2;
 console.log(`Bill = ${bill}\nTip = ${tip}\nTotal = ${bill + tip}`);
 
+###############
+THE STRICT MODE
+###############
 
+Every .js file should start with the statement:
 
+'strict mode';
+
+Strict mode makes several changes to normal JavaScript semantics:
+
+Eliminates some JavaScript silent errors by changing them to throw errors.
+Fixes mistakes that make it difficult for JavaScript engines to perform optimizations: strict mode code can sometimes be made to run faster than identical code that's not strict mode.
+Prohibits some syntax likely to be defined in future versions of ECMAScript.
 
 */
 
